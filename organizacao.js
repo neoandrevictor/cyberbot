84894021eb06fdc440d548d7d2b9e95e834a5006
neoandrevictor <neoandrevictor@hotmail.com>
2019-01-12 20:05:10 -0200

    Add files via upload

---
+++ organizacao.js
@@ -0,0 +1,69 @@
+	function Main()
+
+	//Tabela
+
+	function AdcionaHistorico(id,usuario,pergunta_usuario,frase_limpa_usuario,pergunta_bot,resposta,score,assunto,confianca,cadastro)
+	function AdcionaPerguntaResposta(id,pergunta,resposta,assunto,confianca,cadastro)
+	function AssuntosDiferentes()
+	function RetornaDataHoraAtual()
+
+	//Chat
+
+	function BotEscreve(mensagem)
+	function DefineAssunto(numero)
+	function ForcaLimpaMsgBox()	
+	function LimpraPrimeiroEnter(msg)
+	function MensagemAssuntos(vetorassuntos)	
+	function SelecionarUsuario(usuario)
+
+	//Comandos
+
+	function EnviarComando(msg)
+
+	function COM_assunto(msg)
+	function COM_delete()
+	function COM_delete_especifico(msg)
+	function COM_faqcsv(msg)
+	function COM_import(msg)
+	function COM_login()
+	function COM_update(msg)
+
+	//IA
+
+	function ComparaFrases(frase1,frase2)
+	function EnviarMensagem(event,msg)
+	function EnviarResposta(msg) 
+	function FraseLimpa(frase)
+	function PalavraLimpa(palavra)
+	function PalavrasNaoChave() 
+	function ProcuraRespostaNivel2(msg) 
+
+	//Get-Set
+
+	function Get_Assunto()
+	function Set_Assunto(assunto)
+
+	function Get_Confianca()
+	function Set_Confianca(usuario)
+
+	function Get_Fuzzy_Bool()
+	function Set_Fuzzy_Bool()
+
+	function Get_Numero_de_Linhas_da_Tabela()
+	function Get_Numero_de_Linhas_da_Tabela_Historico()
+
+	function Get_Primeira_Mensagem_Bool()
+	function Set_Primeira_Mensagem_Bool(booleano)
+
+	function Get_Usuario()
+	function Set_Usuario(usuario)
+
+	function TemAssunto()
+
+
+
+
+
+
+
+
